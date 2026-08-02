4acad40b95b43dd2224a9548ce407eed7b057f12
22shub <shubhgupta0022@gmail.com>
2026-08-02 17:37:48 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (56.23%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-     //if array is sortted in non dec. order after rotation there is only one timme a condition 
-     //where element at n+1<ele, at n throught the iteration
+     
      int count =0;
      for(int i=0;i<nums.size()-1;i++){
         if(nums[i]>nums[i+1])count++;
