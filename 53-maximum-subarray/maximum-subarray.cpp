d7a3e43bf626fdb65f5b3928f37f79994110e165
22shub <shubhgupta0022@gmail.com>
2026-08-20 23:58:28 +0530

    Time: 0 ms (100.00%) | Memory: 71.7 MB (81.60%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        int max,sum=INT_MIN;
+            for(int i=0;i<nums.size();i++){
+                max=max+nums[i];
+                if(sum<max)sum=max;
+                if(max<0)max=0;
+            }
+            return sum;
+            //kadens algo best solution
       /*  if(nums.size()==1)return nums[0];
         int l=0,h=nums[0],psum=0,maxsum=nums[0];
         for(int i=0;i<nums.size();i++){
@@ -11,13 +19,7 @@ public:
             }
             return maxsum;
             */
-            int max,sum=INT_MIN;
-            for(int i=0;i<nums.size();i++){
-                max=max+nums[i];
-                if(sum<max)sum=max;
-                if(max<0)max=0;
-            }
-            return sum;
+            //my solution using prefix sum ,same tc and sc as kadanes
         }
         
 };
